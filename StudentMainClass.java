a7658e3a922e6fb1b7f0d98aa6f72c111543f287
jcssona <jcssona@gmail.com>
2026-08-25 22:09:55 +0530

    Removed alll files

--- StudentMainClass.java
+++
@@ -1,8 +0,0 @@
-package assignemntprograms;
-
-public class StudentMainClass {
-    public static void main(String[] args) {
-        Student stud1 = new Student("Jay Sonawane","D-399",70);
-        stud1.displayStudentsData();
-    }
-}
